50226ad57c18de1f238bbdd1c5b28be8ea35e4ee
yutounun <geek.yuto@gmail.com>
2023-02-28 11:11:15 +0900

    ✨ Set deployment workflow in push to main workflow

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App () {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.{bear}
+          Edit <code>src/App.js</code> and save to reload.{bear} test
           <Button variant="contained">button</Button>
         </p>
         <Button onClick={increasePopulation}>Increase</Button>

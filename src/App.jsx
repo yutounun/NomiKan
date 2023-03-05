5937378e4f032b32a9d4c665e40fbd08d915d2f9
Akky-Y <chilcolor.1216a@gmail.com>
2023-03-05 17:15:11 +0900

    ✨ Create InputField component

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,46 @@
-import React from 'react'
-import logo from './logo.svg'
-import './App.css'
-import MyButton from './components/Commons/Atoms/MyButton'
-import Button from '@mui/material/Button'
-import { useStore } from './stores/storeSample.ts'
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import MyButton from "./components/Commons/Atoms/MyButton";
+import InputField from "./components/Commons/Atoms/InputField.tsx";
+import Button from "@mui/material/Button";
+import { useStore } from "./stores/storeSample.ts";
 
 function App() {
   const [bear, removeAllBears, increasePopulation] = useStore((state) => [
     state.bears,
     state.removeAllBears,
     state.increasePopulation,
-  ])
+  ]);
 
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <MyButton
-          variant='contained'
-          color='secondary'
-          size='small'
-          value='John'
+          variant="contained"
+          color="secondary"
+          size="small"
+          value="John"
         />
+        <InputField />
         <p>
           Edit <code>src/App.js</code> and save to reload.{bear} test
-          <Button variant='contained'>button</Button>
+          <Button variant="contained">button</Button>
         </p>
         <Button onClick={increasePopulation}>Increase</Button>
         <Button onClick={removeAllBears}>Clear</Button>
         <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
         >
           Learn React{process.env.REACT_APP_ENV}
         </a>
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Commons/Atoms/InputField.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import TextField from "@mui/material/TextField";
+
+const InputField = () => {
+  return <TextField data-testid="input-field"></TextField>;
+};
+
+export default InputField;

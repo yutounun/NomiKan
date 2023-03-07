3c7ec041acf854a95688ee0db861c205bd7e564e
yutounun <geek.yuto@gmail.com>
2023-03-07 21:07:55 +0900

    Install react-router-dom and add sample page and root page

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
+import Top from "pages/Top";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement,
+  document.getElementById("root") as HTMLElement
 );
+
+const router = createBrowserRouter([
+  {
+    path: "/sample",
+    element: <App />,
+  },
+  {
+    path: "/",
+    element: <Top />,
+  },
+]);
+
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/pages/Top.tsx
@@ -0,0 +1,9 @@
+import Button from "@mui/material/Button";
+import React from "react";
+
+// class Props {}
+function MyButton() {
+  return <Button>aa</Button>;
+}
+
+export default MyButton;

9e89a2506b3832110a24e4d44928f6e9ca98ca87
yutounun <geek.yuto@gmail.com>
2023-03-09 20:57:50 +0900

    ✨ create welcome page

--- src/pages/Top.tsx
+++ src/pages/Top.tsx
@@ -1,9 +1,58 @@
-import Button from "@mui/material/Button";
 import React from "react";
 
-// class Props {}
-function MyButton() {
-  return <Button>aa</Button>;
+import "./Top.css";
+
+import MyButton from "components/Commons/Atoms/MyButton";
+import BaseLayout from "components/Commons/Layout/BaseLayout";
+import MyHeader from "components/Commons/Organisms/MyHeader";
+
+import Cheers from "static/cheers.svg";
+import RocketLaunchOutlinedIcon from "@mui/icons-material/RocketLaunchOutlined";
+import HowToRegOutlinedIcon from "@mui/icons-material/HowToRegOutlined";
+import LoginOutlinedIcon from "@mui/icons-material/LoginOutlined";
+
+function Top() {
+  const clickStartBtn = () => {
+    console.log("clicked!");
+  };
+  return (
+    <BaseLayout>
+      <div className="wrapper">
+        <MyHeader value="NomiKan" />
+        <div className="container">
+          <span className="text-gray top-title">部長のお会計をお助け！</span>
+          <img src={Cheers} alt="" />
+          <span className="explanation">
+            各メンバーの支払額を%で計算することができます
+          </span>
+          <div className="buttons">
+            <MyButton
+              onClick={clickStartBtn}
+              startIcon={<RocketLaunchOutlinedIcon />}
+              variant="outlined"
+              value="はじめる"
+              color="primary"
+              size="large"
+            />
+            <MyButton
+              startIcon={<HowToRegOutlinedIcon />}
+              variant="outlined"
+              value="会員登録"
+              color="primary"
+              size="large"
+            />
+            <MyButton
+              startIcon={<LoginOutlinedIcon />}
+              variant="outlined"
+              value="ログイン"
+              color="primary"
+              size="large"
+            />
+          </div>
+        </div>
+      </div>
+    </BaseLayout>
+  );
 }
 
-export default MyButton;
+export default Top;

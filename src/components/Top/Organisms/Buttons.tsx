05ee35bf9d220f1cad52afc4e407fb84e407c5e6
yutounun <geek.yuto@gmail.com>
2023-03-11 15:18:33 +0900

    Remove unnecasary minute components

--- src/components/Top/Organisms/Buttons.tsx
+++ src/components/Top/Organisms/Buttons.tsx
@@ -1,16 +1,46 @@
 import React from "react";
 
-import StartButton from "components/Top/Atoms/StartButton";
-import SignUpButton from "components/Top/Atoms/SignUpButton";
-import LoginButton from "components/Top/Atoms/LoginButton";
+import RocketLaunchOutlinedIcon from "@mui/icons-material/RocketLaunchOutlined";
+import LoginOutlinedIcon from "@mui/icons-material/LoginOutlined";
+import HowToRegOutlinedIcon from "@mui/icons-material/HowToRegOutlined";
+import MyButton from "components/Commons/Atoms/MyButton";
+import { Link } from "react-router-dom";
 import { Stack } from "@mui/material";
 
 function Buttons() {
+  const buttons = [
+    {
+      to: "/total-amount",
+      label: "はじめる",
+      icon: <RocketLaunchOutlinedIcon />,
+    },
+    {
+      // TODO: update path
+      to: "/total-amount",
+      label: "会員登録",
+      icon: <HowToRegOutlinedIcon />,
+    },
+    {
+      // TODO: update path
+      to: "/total-amount",
+      label: "ログイン",
+      icon: <LoginOutlinedIcon />,
+    },
+  ];
   return (
     <Stack direction="column" gap={2} pt={3} justifyContent="center">
-      <StartButton />
-      <SignUpButton />
-      <LoginButton />
+      {buttons.map((button) => (
+        <Link to={button.to} style={{ textDecoration: "none" }}>
+          <MyButton
+            startIcon={button.icon}
+            variant="outlined"
+            value={button.label}
+            color="primary"
+            size="large"
+            sx={{ borderRadius: "10px", width: "200px" }}
+          />
+        </Link>
+      ))}
     </Stack>
   );
 }

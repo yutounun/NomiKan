7588422b9e09136d690f65d76dc5567501db894a
yutounun <geek.yuto@gmail.com>
2023-03-11 09:22:15 +0900

    💄 update theme color

--- src/theme.ts
+++ src/theme.ts
@@ -1,15 +1,40 @@
 import { createTheme } from "@mui/material/styles";
 
+declare module "@mui/material/styles" {
+  interface Theme {
+    // eslint-disable-next-line @typescript-eslint/ban-types
+    myFontSize: Object;
+  }
+  interface ThemeOptions {
+    // eslint-disable-next-line @typescript-eslint/ban-types
+    myFontSize: Object;
+  }
+}
+interface Theme {
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  myFontSize: Object;
+}
+
 const theme = (isDarkMode: boolean) => {
-  const originalTheme = createTheme({
+  const originalTheme: Theme = createTheme({
     palette: {
       mode: isDarkMode ? "dark" : "light",
       primary: {
-        main: "#00ff62",
+        main: "#d500ff",
       },
       secondary: {
-        main: "#a600ff",
+        main: "#0f0",
       },
+      info: {
+        main: "#8f8f8f",
+      },
+    },
+    myFontSize: {
+      xs: "0.75rem",
+      sm: "0.875rem",
+      md: "1rem",
+      lg: "1.125rem",
+      xl: "1.25rem",
     },
   });
   return originalTheme;

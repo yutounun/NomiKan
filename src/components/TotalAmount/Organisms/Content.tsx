56cdf9787ba887284eaccfe6b7b5bd067a904369
yutounun <geek.yuto@gmail.com>
2023-03-12 15:42:04 +0900

    Create myAlert component

--- src/components/TotalAmount/Organisms/Content.tsx
+++ src/components/TotalAmount/Organisms/Content.tsx
@@ -12,6 +12,8 @@ function Content({ onOpen }: Props) {
   const [cost, setCost] = useCostStore((state) => [state.cost, state.setCost]);
   const [localCost, setLocalCost] = useState<number | string | undefined>("");
 
+  console.log(cost);
+
   /** save cost to Zustand store */
   const handleClickButton = () => {
     // set input cost on Session storage
--- src/pages/TotalAmount.tsx
+++ src/pages/TotalAmount.tsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
-import { Alert, Snackbar, Stack } from "@mui/material";
+import { Stack } from "@mui/material";
 
 import BaseLayout from "components/Commons/Layout/BaseLayout";
 import MyHeader from "components/Commons/Organisms/MyHeader";
 import MyStepper from "components/Commons/Organisms/MyStepper";
 import Content from "components/TotalAmount/Organisms/Content";
+import MyAlert from "components/Commons/Molecules/MyAlert";
 
 function TotalAmount() {
   const [open, setOpen] = useState(false);
@@ -13,29 +14,21 @@ function TotalAmount() {
     setOpen(true);
   };
 
-  /** close the snack bar */
-  const handleOnClose = () => {
-    setOpen(false);
-  };
-
   return (
     <BaseLayout>
       <Stack direction="column" justifyContent="center" alignItems="center">
+        {/* 合計金額Header */}
         <MyHeader value="合計金額" />
-        <MyStepper activeStep={0} />
-        <Content onOpen={handleOnOpen} />
-      </Stack>
 
-      {/* Alert for the completion of registration  */}
-      <Snackbar
-        anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
-        open={open}
-        onClose={handleOnClose}
-      >
-        <Alert onClose={handleOnClose} severity="info" sx={{ width: "100%" }}>
-          登録しました！
-        </Alert>
-      </Snackbar>
+        {/* Stepper showing the current page */}
+        <MyStepper activeStep={0} />
+
+        {/* Content including input and button */}
+        <Content onOpen={handleOnOpen} />
+
+        {/* Alert for the completion of registration  */}
+        <MyAlert parentOpen={open} value="登録が完了しました！" />
+      </Stack>
     </BaseLayout>
   );
 }

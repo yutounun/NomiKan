aa8b2060c1c98a73b5394335a1b3ec4dd0cd1b9b
yutounun <geek.yuto@gmail.com>
2023-03-12 15:19:15 +0900

    Explicit content component from totalAMount page

--- src/pages/TotalAmount.tsx
+++ src/pages/TotalAmount.tsx
@@ -3,27 +3,14 @@ import { Alert, Snackbar, Stack } from "@mui/material";
 
 import BaseLayout from "components/Commons/Layout/BaseLayout";
 import MyHeader from "components/Commons/Organisms/MyHeader";
-
-import MyButton from "components/Commons/Atoms/MyButton";
-import { useCostStore } from "stores/cost";
-import CostTextField from "components/TotalAmount/Atoms/CostTextField";
 import MyStepper from "components/Commons/Organisms/MyStepper";
+import Content from "components/TotalAmount/Organisms/Content";
 
 function TotalAmount() {
-  const [cost, setCost] = useCostStore((state) => [state.cost, state.setCost]);
   const [open, setOpen] = useState(false);
-  const [localCost, setLocalCost] = useState<number | string | undefined>("");
 
-  /** save cost to Zustand store */
-  const handleClickButton = () => {
-    // set input cost on Session storage
-    setCost(localCost);
-
-    // open the snack bar
+  const handleOnOpen = () => {
     setOpen(true);
-
-    // clear input cost
-    setLocalCost("");
   };
 
   /** close the snack bar */
@@ -31,32 +18,12 @@ function TotalAmount() {
     setOpen(false);
   };
 
-  /** handle input cost */
-  const handleUpdateCost = (updatedCost: number | string | undefined) => {
-    setLocalCost(updatedCost);
-  };
-
   return (
     <BaseLayout>
       <Stack direction="column" justifyContent="center" alignItems="center">
         <MyHeader value="合計金額" />
         <MyStepper activeStep={0} />
-        <Stack
-          direction="column"
-          justifyContent="center"
-          mt={10}
-          alignItems="center"
-          gap="70px"
-        >
-          <CostTextField onChangeCost={handleUpdateCost} value={localCost} />
-          <MyButton
-            onClick={handleClickButton}
-            disabled={localCost === ""}
-            value="登録"
-            variant="contained"
-            sx={{ borderRadius: "30px", width: "100%" }}
-          />
-        </Stack>
+        <Content onOpen={handleOnOpen} />
       </Stack>
 
       {/* Alert for the completion of registration  */}

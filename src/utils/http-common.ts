acb04c974197497fe234288c692330bfdc092355
yutounun <geek.yuto@gmail.com>
2023-04-02 14:25:23 +0900

    fix lint error

--- src/utils/http-common.ts
+++ src/utils/http-common.ts
@@ -1,8 +1,8 @@
-import axios from 'axios'
+import axios from "axios";
 
 export default axios.create({
   baseURL: process.env.REACT_APP_ENV,
   headers: {
-    'Content-type': 'application/json',
+    "Content-type": "application/json",
   },
-})
+});
